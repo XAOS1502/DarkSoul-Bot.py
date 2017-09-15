e452b4ca906e8bc49741b58468670f17c3f9499c
「XΛOS」 <30846531+XAOS1502@users.noreply.github.com>
2017-09-16 01:43:43 +0530

    fixed

--- bot.py
+++ bot.py
@@ -8,8 +8,15 @@ async def on_ready():
   print('Bot is online!')
 
 @bot.command()
-async def ping(self, ctx):
-  await ctx.send('Pong!')
+async def ping(ctx):
+  await bot.say('Pong!')
+  
+@bot.event
+async def on_message(message):
+    if "(╯°□°）╯︵ ┻━┻" in message.content:
+      await bot.send_message(message.channel, "┬─┬﻿ ノ( ゜-゜ノ)")
+
+    await bot.process_commands(message)
 
 bot.run('MzU4MzAwNjE3NjY1NTQ0MTk0.DJ2kbw.YR9Rr0VuHk0vy9B6z_Uu-AT5AWA')
  
