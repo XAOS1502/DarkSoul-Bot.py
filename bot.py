37fe6357389e0d72d4dcef650ea5da5326942663
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2017-09-16 11:33:32 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,25 @@ bot = commands.Bot(command_prefix='?')
 async def on_ready():
   print('Bot is online!')
 
-@bot.command()
+@bot.command(name='presence')
+async def _set(Type=None,*,thing=None):
+    """Change the bot's discord game/stream!"""
+    if Type is None:
+            await bot.say('Usage: `.presence [game/stream] [message]`')
+    else:
+        if Type.lower() == 'stream':
+            await bot.change_presence(game=discord.Game(name=thing,type=1,url='https://www.twitch.tv/a'),status='online')
+            await bot.say('Set presence to. `Streaming {}`'.format(thing))
+        elif Type.lower() == 'game':
+            await bot.change_presence(game=discord.Game(name=thing))
+            await bot.say('Set presence to `Playing {}`'.format(thing))
+        elif Type.lower() == 'clear':
+            await bot.change_presence(game=None)
+            await bot.say('Cleared Presence')
+        else:
+            await bot.say('Usage: `.presence [game/stream] [message]`')
+  
+  @bot.command()
 async def ping(ctx):
     """Pong! Returns your websocket latency."""
     em = discord.Embed()
