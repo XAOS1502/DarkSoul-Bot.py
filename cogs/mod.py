c957e27b4edfaa06d7153b636d07e353a5ffaf24
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2017-09-17 12:31:39 -0800

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,1 +1,13 @@
+    @commands.command()
+    async def kick(self, ctx, member : discord.Member, *, reason=None):
+        '''Kick someone from the server.'''
+        try:
+            await ctx.guild.kick(member, reason=reason)
+        except:
+            success = False
+        else:
+            success = True
 
+        emb = await self.format_mod_embed(ctx, member, success, 'kick')
+
+        await ctx.send(embed=emb)
